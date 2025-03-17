de6437c4a5742125945c999b087f21b3a4789f51
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-17 22:58:58 +0530

    Swagger added

---
+++ src/main/java/com/example/AddressBook/config/swaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.AddressBook.config;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class swaggerConfig{
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Address book")
+                        .version("1.0")
+                        .description("Api docs for address book")
+                );
+    }
+}
