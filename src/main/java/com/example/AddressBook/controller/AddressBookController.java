ea1a55a1195c15f8c7ae079f8eb95eabf6a8b075
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-17 22:43:17 +0530

    Logging added

--- src/main/java/com/example/AddressBook/controller/AddressBookController.java
+++ src/main/java/com/example/AddressBook/controller/AddressBookController.java
@@ -4,6 +4,7 @@ import com.example.AddressBook.dto.AddressBookDto;
 import com.example.AddressBook.model.AddressBook;
 import com.example.AddressBook.repository.AddressBookRepository;
 import com.example.AddressBook.services.AddressBookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/addressbook")
 public class AddressBookController {
@@ -19,26 +21,32 @@ public class AddressBookController {
 
     @PostMapping("/add")
     public AddressBook addAddressBook(@RequestBody AddressBookDto addressBookDto){
+        log.info("Request for adding new Address Book");
         return  addressBookService.addAddressBook(addressBookDto);
     }
 
     @GetMapping("/get")
     public List<AddressBook> getAllAddressBook(){
+        log.info("Request for getting all the Address Book");
         return addressBookService.getAllAddressBook();
     }
 
     @GetMapping("/get/{id}")
+
     public AddressBook getAddressBookById(@PathVariable Long id){
+        log.info("Request for finding Address Book with id:{}",id);
         return addressBookService.getAddressBookById(id);
     }
     @PutMapping("/Update/{id}")
     public AddressBook updateAddressBook(@PathVariable Long id, @RequestBody AddressBookDto addressBookDto){
+        log.info("Request for updating Address Book with id:{}",id);
         return addressBookService.updateAddressBookById(id,addressBookDto);
     }
 
     @DeleteMapping("/delete/{id}")
 
     public String deleteAddressBook(@PathVariable Long id) {
+        log.info("Request for deleting Address Book with id:{}",id);
         addressBookService.deleteAddressBookById(id);
         return "Address book entry deleted successfully.";
     }
--- src/main/java/com/example/AddressBook/services/AddressBookService.java
+++ src/main/java/com/example/AddressBook/services/AddressBookService.java
@@ -4,11 +4,13 @@ import com.example.AddressBook.dto.AddressBookDto;
 import com.example.AddressBook.model.AddressBook;
 import com.example.AddressBook.repository.AddressBookRepository;
 import com.example.AddressBook.serviceInterfaces.AddressBookInterface;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class AddressBookService implements AddressBookInterface {
 
@@ -16,39 +18,50 @@ public class AddressBookService implements AddressBookInterface {
     AddressBookRepository addressBookRepository;
     @Override
     public AddressBook addAddressBook(AddressBookDto addressBookDto){
+        log.info("Adding new AddressBook {}");
         AddressBook addressBook=new AddressBook();
         addressBook.setName(addressBookDto.getName());
         addressBook.setEmail(addressBookDto.getEmail());
         addressBook.setPhone(addressBookDto.getPhone());
-        return addressBookRepository.save(addressBook);
+        AddressBook savedAddressBook= addressBookRepository.save(addressBook);
+        log.info("New AddressBook details saved in db {}");
+        return savedAddressBook;
     }
     @Override
     public List<AddressBook> getAllAddressBook(){
-        return addressBookRepository.findAll();
+        log.info("Fetching all the AddressBook {}");
+        List<AddressBook> addressBooks=addressBookRepository.findAll();
+        log.info("Fetching done ");
+        return addressBooks;
     }
 
     @Override
     public  AddressBook getAddressBookById(Long id){
+        log.info("find details of AddressBook with id ");
         return addressBookRepository.findById(id).orElseThrow(()-> new RuntimeException("Id not found"+id));
     }
 
     @Override
     public AddressBook updateAddressBookById(Long id, AddressBookDto addressBookDto){
+        log.info("find details of AddressBook with id ");
         AddressBook found=addressBookRepository.findById(id).orElseThrow(()->new RuntimeException("Id not found"));
         found.setName(addressBookDto.getName());
         found.setPhone(addressBookDto.getPhone());
         found.setEmail(addressBookDto.getEmail());
 
-        return addressBookRepository.save(found);
-
+        AddressBook uodate=addressBookRepository.save(found);
+        log.info("Update done");
+        return uodate;
 
 
     }
 
     @Override
     public void deleteAddressBookById(Long id){
+        log.info("find details of AddressBook with id ");
         AddressBook found=addressBookRepository.findById(id).orElseThrow(() -> new RuntimeException("Id not found: " + id));
 
         addressBookRepository.delete(found);
+        log.info(" AddressBook deleted successfully");
     }
 }
